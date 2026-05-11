69c39aa938c4864d150f3622e54de299526632ef
alejandro21rg <ruiz32909@gmail.com>
2026-05-11 17:30:10 +0200

    Eda_11_05_1

---
+++ Código/código/codigo.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importamos el tsv
+df_netflix_country_weekly = pd.read_csv("Código/data/most-popular_country_weekly.tsv", sep="\t")
+
+df_netflix_country_weekly
+
+
+
+
+
+
+
+
+
+
